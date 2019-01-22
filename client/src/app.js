b388d5dd2a67ca01eb2d7b3d2268533439faea4c
JecoLantern <jbayonke@hotmail.com>
2019-01-21 20:54:43 -0800

    initial

--- client/src/app.js
+++ client/src/app.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
+import Wrapper from "./components/Wrapper";
+import NavBar from "./components/NavBar";
 import { hot } from "react-hot-loader";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 
 class App extends Component {
     render() {
         return (
             <div>
+                <NavBar />
+                <Wrapper>
                 <Router>
                     <Switch>
 
                         <Route />
                     </Switch>
                 </Router>
+                </Wrapper>
             </div>
         )
     }
