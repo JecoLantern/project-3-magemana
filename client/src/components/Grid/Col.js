2fbab157d5ff8e91287e415466eea3a6f6287c3c
Austin <Austin.shepherd.96@gmail.com>
2019-01-28 00:21:43 -0800

    Folder structure fix, Random name list started in sruay folder

--- client/src/components/Grid/Col.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Col = props => (
-    <div className={`col-${props.size}`}>
-        {props.children}
-    </div>
-)
-
-export default Col;
---
+++ client/src/pages/Survey/components/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Col} from '../../../Grid'
+
+questionBox = props =>{
+    return (
+        <Col size="m-4">
+        </Col>
+    )
+}
