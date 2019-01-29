275dfada263f4beb6bf909c973a08151a857abb7
Austin <Austin.shepherd.96@gmail.com>
2019-01-28 18:44:33 -0800

    Survey fixed

--- client/src/pages/Survey/index.js
+++ client/src/pages/Survey/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react-dom';
 import { Col, Row } from '../../components/Grid';
-import {questionBox} from './components'
+import questionBox from './components';
 import './_surveyStyle.css';
 import ranNames from './util/randomNames.json';
 
