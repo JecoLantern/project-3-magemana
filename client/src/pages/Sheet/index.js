6045eb48ce15f10b95dcc3fedba9d13e2f9b9715
JecoLantern <jbayonke@hotmail.com>
2019-01-28 18:02:31 -0800

    charsheet in progress

--- client/src/pages/Sheet/index.js
+++ client/src/pages/Sheet/index.js
@@ -1,1 +1,49 @@
 // export { default } from "../../components";
+import React, { Component } from "react-dom";
+import {Col, Row} from "../../components/Grid";
+import "../Sheet/_charSheetSty.css";
+
+class CharSheet {
+
+    render() {
+        return (
+            <div className="container">
+                <Row>
+                    <Col size="1, sm-1, md-1, l-1, xl-1">
+                        <div id="diamond1"><h6>Place Logo IMG here</h6></div>
+                    </Col>
+                    <Col size="1, sm-1, md-1, l-1, xl-1">
+                        <div id="square1"><h6>Avatar dummy IMG</h6></div>
+                    </Col>
+                    <Col size="10, sm-10, md-10, l-10, xl-10">
+                        <Row>
+                            <Col size="4, sm-4, md-4, l-4, xl-4">
+                                <div><h6>Namae wa</h6></div>
+                            </Col>
+                            <Col size="4, sm-4, md-4, l-4, xl-4">
+                                <div><h6>Background</h6></div>
+                            </Col>
+                            <Col size="4, sm-4, md-4, l-4, xl-4">
+                                <div><h6>Experience Points</h6></div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col size="4, sm-4, md-4, l-4, xl-4">
+                                <div><h6>Race</h6></div>
+                            </Col>
+                            <Col size="4, sm-4, md-4, l-4, xl-4">
+                                <div><h6>Alignment</h6></div>
+                            </Col>
+                            <Col size="4, sm-4, md-4, l-4, xl-4">
+                                <div><h6>Class & Level</h6></div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
+
+};
+
+export default CharSheet;
