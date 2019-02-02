3a6d8f7e3a489e537a0cc51d48100e899e086f88
TroyAG <gordontaaj@gmail.com>
2019-02-02 13:01:25 -0800

    fixed the select page rendering and adjusted the css accordingly

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const path = require("path");
-
 const mongoose = require('mongoose');
 const session = require('express-session');
 const routes = require("./routes");
 const passport = require("passport");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
