99d68a7ce14d73771e6903d16292fa6fa27ed1cb
JecoLantern <jbayonke@hotmail.com>
2019-02-04 21:42:10 -0800

    skillsblock comp

---
+++ client/src/pages/Sheet/components/skillsBlock.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Col, Row } from '../../../components/Grid';
+import List from '../../../components/Lists';
+import { Skill } from "../../../components/Lists/ListItems";
+import '../styles/skillsBlock.css';
+
+const SkillsBlock = () => (
+    <Col size="4, sm-4, md-4, lg-4, xl-4">
+        <div id="squareTV7">
+            <List>
+                <Skill></Skill>
+            </List>
+        </div>
+    </Col>
+)
+
+export default SkillsBlock;
