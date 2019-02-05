fd987e630f43aee14022f813f1f3ad1ee6ec1987
JecoLantern <jbayonke@hotmail.com>
2019-02-04 21:40:33 -0800

    hpblock comp

---
+++ client/src/pages/Sheet/components/hpBlock.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Col, Row} from '../../../components/Grid';
+import '../styles/hpBlock.css';
+
+const HpBlock = () => (
+    <Row>
+        <Col size="6, sm-6, md-6, lg-6, xl-6">
+            <div id="squareTV4">
+                <div id="rectangleRounded4"></div>
+            </div>
+        </Col>
+        <Col size="6, sm-6, md-6, lg-6, xl-6">
+            <div id="squareTV5">
+                <div id="rectangleRounded5"></div>
+            </div>
+        </Col>
+        <div id="rectangleRounded6"></div>
+    </Row>
+)
+
+export default HpBlock;
