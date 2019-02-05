b385615b513277074634a3f560810ba7867cb21d
TroyAG <gordontaaj@gmail.com>
2019-02-04 18:54:04 -0800

    rollback

--- client/src/pages/Select/components/header.jsx
+++ client/src/pages/Select/components/header.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import logo from '../../../components/assets/favicon.ico'
-=======
-import logo from '../assets/favicon.ico'
->>>>>>> 1c2994b7c6b09c5d5fb7545c5b02e474f635b534
 
 const Header = () => (
     <div className="App-header container-full">
