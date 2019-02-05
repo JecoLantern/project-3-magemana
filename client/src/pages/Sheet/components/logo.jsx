94594e08edc25a8f3bdc9b118870f358e8b291fe
JecoLantern <jbayonke@hotmail.com>
2019-02-04 21:31:56 -0800

    logo component

---
+++ client/src/pages/Sheet/components/logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Col} from '../../../components/Grid';
+import '../styles/logo.css';
+
+const Logo = () => (
+    <Col size="2, sm-2, md-2, lg-2, xl-2">
+        <div id="diamond1"><h6>Place Logo IMG here</h6></div>
+    </Col>
+)
+
+export default Logo;
