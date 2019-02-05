9730da03d52a0232ee6fb1564f4c6e5e5e9c0fad
JecoLantern <jbayonke@hotmail.com>
2019-02-04 21:29:18 -0800

    initial folders convert to react comp

--- client/src/pages/Sheet/index.js
+++ client/src/pages/Sheet/index.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
-import {Col, Row, Container} from "../../components/Grid";
+import { Col, Row, Container } from "../../components/Grid";
 import List from "../../components/Lists";
-import {Equip, Skill} from "../../components/Lists/ListItems";
+import { Equip, Skill } from "../../components/Lists/ListItems";
 import "../Sheet/_charSheetSty.css";
+
+//Importing CharSheet Components
+import Logo from "./components/logo";
+import Avatar from "./components/avatar";
+import NamePlate from "./components/namePlate";
+import AttributeBlock from "./components/attributeBlock";
+import VitalBlock from "./components/vitalBlock";
+import TertiaryAttribute from "./components/tertiaryAttribute";
+import CharacterFeatures from "./components/charFeatures";
+import HpBlock from "./components/hpBlock";
+import HitDice from "./components/hitDice";
+import SkillsBlock from "./components/skillsBlock";
+import Equipment from "./components/equipment";
 
 class CharacterSheet extends Component{
 
@@ -10,215 +23,41 @@ class CharacterSheet extends Component{
         return (
             <Container>
                 <Row>
-                    <Col size="2, sm-2, md-2, lg-2, xl-2">
-                        <div id="diamond1"><h6>Place Logo IMG here</h6></div>
-                    </Col>
-                    <Col size="2, sm-2, md-2, lg-2, xl-2">
-                        <div id="square1"><h6>Avatar dummy IMG</h6></div>
-                    </Col>
-                    <Col size="8, sm-8, md-8, lg-8, xl-8">
-                        <Row className="rowA1">
-                            <Col size="4, sm-4, md-4, lg-4, xl-4">
-                                <div className="name"><h2>Namae wa</h2></div>
-                            </Col>
-                            <Col size="4, sm-4, md-4, lg-4, xl-4">
-                                <div className="background"><h2>Background</h2></div>
-                            </Col>
-                            <Col size="4, sm-4, md-4, lg-4, xl-4">
-                                <div className="expPoints"><h2>Experience Points</h2></div>
-                            </Col>
-                        </Row>
-                        <Row className="rowA2">
-                            <Col size="4, sm-4, md-4, lg-4, xl-4">
-                                <div className="race"><h2>Race</h2></div>
-                            </Col>
-                            <Col size="4, sm-4, md-4, lg-4, xl-4">
-                                <div className="alignment"><h2>Alignment</h2></div>
-                            </Col>
-                            <Col size="4, sm-4, md-4, lg-4, xl-4">
-                                <div className="classLvl"><h2>Class & Level</h2></div>
-                            </Col>
-                        </Row>
-                    </Col>
+                    <Logo></Logo>
+                    <Avatar></Avatar>
+                    <NamePlate></NamePlate>
                 </Row>
                 <Row>
                     <Col size="6, sm-6, md-6, lg-6, xl-6">
                         <Row>
-                            <Col size="2, sm-2, md-2, lg-2, xl-2">
-                                <div>
-                                    <div id="rectangle"></div>
-                                    <div id="squareTV">
-                                        <div id="rectangleRounded"></div>
-                                    </div>
-                                    <div id="oval">
-                                        <div id="circle"></div>
-                                    </div>
-                                </div>
-                            </Col>
-                            <Col size="2, sm-2, md-2, lg-2, xl-2">
-                                <div>
-                                    <div id="rectangle"></div>
-                                    <div id="squareTV">
-                                        <div id="rectangleRounded"></div>
-                                    </div>
-                                    <div id="oval">
-                                        <div id="circle"></div>
-                                    </div>
-                                </div>
-                            </Col>
-                            <Col size="2, sm-2, md-2, lg-2, xl-2">
-                                <div>
-                                    <div id="rectangle"></div>
-                                    <div id="squareTV">
-                                        <div id="rectangleRounded"></div>
-                                    </div>
-                                    <div id="oval">
-                                        <div id="circle"></div>
-                                    </div>
-                                </div>
-                            </Col>
-                            <Col size="2, sm-2, md-2, lg-2, xl-2">
-                                <div>
-                                    <div id="rectangle"></div>
-                                    <div id="squareTV">
-                                        <div id="rectangleRounded"></div>
-                                    </div>
-                                    <div id="oval">
-                                        <div id="circle"></div>
-                                    </div>
-                                </div>
-                            </Col>
-                            <Col size="2, sm-2, md-2, lg-2, xl-2">
-                                <div>
-                                    <div id="rectangle"></div>
-                                    <div id="squareTV">
-                                        <div id="rectangleRounded"></div>
-                                    </div>
-                                    <div id="oval">
-                                        <div id="circle"></div>
-                                    </div>
-                                </div>
-                            </Col>
-                            <Col size="2, sm-2, md-2, lg-2, xl-2">
-                                <div>
-                                    <div id="rectangle"></div>
-                                    <div id="squareTV">
-                                        <div id="rectangleRounded"></div>
-                                    </div>
-                                    <div id="oval">
-                                        <div id="circle"></div>
-                                    </div>
-                                </div>
-                            </Col>
+                            <AttributeBlock></AttributeBlock>
+                            <AttributeBlock></AttributeBlock>
+                            <AttributeBlock></AttributeBlock>
+                            <AttributeBlock></AttributeBlock>
+                            <AttributeBlock></AttributeBlock>
+                            <AttributeBlock></AttributeBlock>
                         </Row>
                     </Col>
                     <Col size="6, sm-6, md-6, lg-6, xl-6">
                     <Row>
-                        <Col size="3, sm-3, md-3, lg-3, xl-3">
-                            <div id="hexagon1">
-                                <div id="hexagon2"></div>
-                            </div>
-                        </Col>
-                        <Col size="3, sm-3, md-3, lg-3, xl-3">
-                            <div id="hexagon3">
-                                <div id="hexagon4"></div>
-                            </div>
-                        </Col>
-                        <Col size="3, sm-3, md-3, lg-3, xl-3">
-                            <div id="hexagon5">
-                                <div id="hexagon6"></div>
-                            </div>
-                        </Col>
-                        <Col size="3, sm-3, md-3, lg-3, xl-3">
-                            <Row>
-                                <div id="squareTV2">
-                                    <div id="rectangleRounded2"></div>
-                                </div>
-                            </Row>
-                            <Row>
-                                <div id="squareTV3">
-                                    <div id="rectangleRounded3"></div>
-                                </div>
-                            </Row>
-                        </Col>
+                        <VitalBlock></VitalBlock>
+                        <VitalBlock></VitalBlock>
+                        <VitalBlock></VitalBlock>
+                        <TertiaryAttribute></TertiaryAttribute>
                     </Row>
                     </Col>
                 </Row>
                 <Row>
                     <Col size="9, sm-9, md-9, lg-9, xl-9">
                     <Row>
-                        <Col size="4, sm-4, md-4, lg-4, xl-4">
-                            <div id="squareTV7">
-                                <List>
-                                    <Skill></Skill>
-                                </List>
-                            </div>
-                        </Col>
-                        <Col size="4, sm-4, md-4, lg-4, xl-4">
-                            <div id="squareTV8">
-                            <Row>
-                                <div id="rectangle2">
-                                <Row>
-                                    <Col size="4, sm4, md-4, lg-4, xl-4">
-                                        <div id="rectangle4"></div>
-                                    </Col>
-                                    <Col size="4, sm4, md-4, lg-4, xl-4">
-                                        <div id="rectangle5"></div>
-                                    </Col>
-                                    <Col size="4, sm4, md-4, lg-4, xl-4">
-                                        <div id="rectangle6"></div>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col size="4, sm4, md-4, lg-4, xl-4">
-                                        <div id="rectangle7"></div>
-                                    </Col>
-                                    <Col size="4, sm4, md-4, lg-4, xl-4">
-                                        <div id="rectangle8"></div>
-                                    </Col>
-                                    <Col size="4, sm4, md-4, lg-4, xl-4">
-                                        <div id="rectangle9"></div>
-                                    </Col>
-                                </Row>
-                                </div>
-                                <div id="rectangle3"></div>
-                            </Row>
-                            <Row>
-                                <div className="space">
-                                    <List>
-                                        <Equip></Equip>
-                                    </List>
-                                </div>
-                            </Row>
-                            </div>
-                        </Col>
-                        <Col size="4, sm-4, md-4, lg-4, xl-4">
-                            <div id="squareTV9"></div>
-                        </Col>
+                        <SkillsBlock></SkillsBlock>
+                        <Equipment></Equipment>
+                        <CharacterFeatures></CharacterFeatures>
                     </Row>
                     </Col>
                     <Col size="3, sm-3, md-3, lg-3, xl-3">
-                        <Row>
-                            <Col size="6, sm-6, md-6, lg-6, xl-6">
-                                <div id="squareTV4">
-                                    <div id="rectangleRounded4"></div>
-                                </div>
-                            </Col>
-                            <Col size="6, sm-6, md-6, lg-6, xl-6">
-                                <div id="squareTV5">
-                                    <div id="rectangleRounded5"></div>
-                                </div>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <div id="rectangleRounded6"></div>
-                        </Row>
-                        <Row>
-                            <div id="squareTV6">
-                                <div id="rectangleRounded7"></div>
-                                <div id="rectangleRounded8"></div>
-                            </div>
-                        </Row>
+                        <HpBlock></HpBlock>
+                        <HitDice></HitDice>
                     </Col>
                 </Row>
             </Container>
