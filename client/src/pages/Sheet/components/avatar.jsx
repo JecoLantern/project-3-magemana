7180d0018e452e878a8c2a413cda3d97eb452699
JecoLantern <jbayonke@hotmail.com>
2019-02-04 21:33:09 -0800

    avatar comp

---
+++ client/src/pages/Sheet/components/avatar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Col} from '../../../components/Grid';
+import '../styles/avatar.css';
+
+const Avatar = () => (
+    <Col size="2, sm-2, md-2, lg-2, xl-2">
+        <div id="square1"><h6>Avatar dummy IMG</h6></div>
+    </Col>
+)
+
+export default Avatar;
