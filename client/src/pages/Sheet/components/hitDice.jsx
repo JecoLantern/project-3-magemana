f5f3f9e68884073c2b03916d9e876b2490bf3842
JecoLantern <jbayonke@hotmail.com>
2019-02-04 21:41:26 -0800

    hitdice comp

---
+++ client/src/pages/Sheet/components/hitDice.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Row} from '../../../components/Grid';
+import '../styles/hitDice.css';
+
+const HitDice = () => (
+    <Row>
+        <div id="squareTV6">
+            <div id="rectangleRounded7"></div>
+            <div id="rectangleRounded8"></div>
+        </div>
+    </Row>
+)
+
+export default HitDice;
