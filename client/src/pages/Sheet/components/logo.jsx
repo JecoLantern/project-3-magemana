1df193799044947c3a568eec4e558e79cafedd90
JecoLantern <jbayonke@hotmail.com>
2019-02-06 20:31:33 -0800

    css color, image fix

--- client/src/pages/Sheet/components/logo.jsx
+++ client/src/pages/Sheet/components/logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Col} from '../../../components/Grid';
 import '../styles/logo.css';
+// import Image from 'react-bootstrap/Image';
+import LogoIMG from '../assets/logoIMG'
 
 const Logo = () => (
     <Col size="2, sm-2, md-2, lg-2, xl-2">
-        <div id="diamond1"><h6>Place Logo IMG here</h6></div>
+        <div id="diamond1"><h6><LogoIMG></LogoIMG></h6></div>
     </Col>
 )
 
