e8c117bc36ddf5d7264efe54e3ff12b9c2017517
TroyAG <gordontaaj@gmail.com>
2019-02-09 13:53:34 -0800

    working character selector

--- client/src/pages/Select/components/header.jsx
+++ client/src/pages/Select/components/header.jsx
@@ -4,7 +4,7 @@ import logo from '../../../components/assets/favicon.ico'
 const Header = () => (
     <div className="App-header container-full">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Characters</h1>
+        <h1 className="App-title"></h1>
     </div>
 )
 
