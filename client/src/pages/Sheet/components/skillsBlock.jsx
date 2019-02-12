523b4c085f48bb927f8e0818786815eb84936da8
JecoLantern <jbayonke@hotmail.com>
2019-02-11 19:44:52 -0800

    added media screen 768, 1024 landscape done

--- client/src/pages/Sheet/components/skillsBlock.jsx
+++ client/src/pages/Sheet/components/skillsBlock.jsx
@@ -5,7 +5,7 @@ import { Skill } from "../../../components/Lists/ListItems";
 import '../styles/skillsBlock.css';
 
 const SkillsBlock = () => (
-    <Col size="4, sm-4, md-4, lg-4">
+    <Col size="4, sm-4, md-4">
         <div id="squareTV7">
             <List>
                 <Skill></Skill>
