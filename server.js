3bd3c4c154e60d3daac1d0449c199efd781633d9
Austin <Austin.shepherd.96@gmail.com>
2019-02-13 19:18:08 -0800

    fuck

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session({
 
 // Serve up static assets (usually on heroku)=============
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 }
 
 // ===== Passport ====
