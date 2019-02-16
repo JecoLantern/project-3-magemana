86897f8cb9e5eff9c755376b929f059902802bf3
JecoLantern <jbayonke@hotmail.com>
2019-02-16 14:48:59 -0800

    death save mod

--- client/src/pages/Sheet/components/hitDice.jsx
+++ client/src/pages/Sheet/components/hitDice.jsx
@@ -4,9 +4,9 @@ import '../styles/hitDice.css';
 
 const HitDice = (props) => (
     <div id="squareTV6">
-    <h5>Hit Dice:</h5>
+    <p className="hitdiceTxt">Hit Dice:</p>
         <div id="rectangleRounded7">Max:{props.hitDice}</div>
-        <div id="rectangleRounded8">Current: <input/></div>
+        <div id="rectangleRounded8">Current: <input className="hitdiceInput"/></div>
     </div>
 )
 
