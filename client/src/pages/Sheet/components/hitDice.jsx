07c4baced745fca6d61b3f88a2c9fbf58153f121
JecoLantern <jbayonke@hotmail.com>
2019-02-18 09:20:08 -0800

    hitdice mod

--- client/src/pages/Sheet/components/hitDice.jsx
+++ client/src/pages/Sheet/components/hitDice.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-// import {Row} from '../../../components/Grid';
 import '../styles/hitDice.css';
 
 const HitDice = (props) => (
     <div id="squareTV6">
     <p className="hitdiceTxt">Hit Dice:</p>
-        <div id="rectangleRounded7">Max:{props.hitDice}</div>
-        <div id="rectangleRounded8">Current: <input className="hitdiceInput"/></div>
+        <div id="rectangleRounded7"><h6 className="hitdiceMaxTxt">Max:</h6><h6 className="hitdiceVal">{props.hitDice}</h6></div>
+        <div id="rectangleRounded8"><h6 className="hitdiceCurrentTxt">Current: </h6><input className="hitdiceInput"/></div>
     </div>
 )
 
--- client/src/pages/Sheet/components/hpBlock.jsx
+++ client/src/pages/Sheet/components/hpBlock.jsx
@@ -5,8 +5,8 @@ import '../styles/hpBlock.css';
 const HpBlock = (props) => (
     <div className="hpCentral">
         <div id="squareTV4"><p className="healthTxt">HEALTH</p>
-            <div id="rectangleRounded4"><h6>HP Max:{props.health}</h6></div>
-            <div id="rectangleRounded5"><h6>HP current:<input className="hpInput"/></h6></div>
+            <div id="rectangleRounded4"><h6 className="hpMaxTxt">HP Max:{props.health}</h6></div>
+            <div id="rectangleRounded5"><h6 className="hpCurrentTxt">HP current:<input className="hpInput"/></h6></div>
         </div>
         <div id="rectangleRounded6">
             <Row>
